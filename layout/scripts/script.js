f7a73d61d8e89816b6396f6599985013071a36e2
NataliaLopatkina <51717101+NataliaLopatkina@users.noreply.github.com>
2020-01-21 15:30:24 +0300

    Add adaptive

--- layout/scripts/script.js
+++ layout/scripts/script.js
@@ -197,3 +197,36 @@ class Slider {
 }
 
 const slider = new Slider();
+
+// Open/close menu
+
+class Menu {
+    constructor() {
+        this.menu = document.querySelector('.nav-small-device__menu');
+        this.buttonMenu = document.querySelector('.button-menu');
+        this.menuList = document.querySelector('.nav-small-device__list');
+
+        this.toggleMenu();
+    }
+
+    toggleMenu() {
+        this.buttonMenu.addEventListener('click', ()=> {
+            if (this.menu.classList.contains('is-active')) {
+                this.menu.classList.remove('is-active');
+                this.buttonMenu.classList.remove('button-menu--closed');
+                this.menu.style.height = 0;
+            } else {
+                this.menu.classList.add('is-active');
+                this.buttonMenu.classList.add('button-menu--closed');
+                this.addHeight();
+            }
+        }) 
+    }
+
+    addHeight() {
+        const heightMenu = this.menuList.offsetHeight;
+        this.menu.style.height = heightMenu + 'px';
+    }
+}
+
+const menu = new Menu();
